5a86cc6037f71bee7b203501e7d73341f25b6582
Liza Star <lizadstar18@gmail.com>
2020-03-23 19:23:44 -0500

    axios call and display cows

--- src/javascripts/helpers/data/cowData.js
+++ src/javascripts/helpers/data/cowData.js
@@ -2,7 +2,18 @@ import axios from 'axios';
 import apiKeys from '../apiKeys.json';
 
 const baseUrl = apiKeys.firebaseKeys.databaseURL;
-
-const getCows = () => axios.get(`${baseUrl}/cows.json`);
+const getCows = () => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/cows.json`)
+    .then((response) => {
+      const demCows = response.data;
+      const cows = [];
+      Object.keys(demCows).forEach((cowId) => {
+        demCows[cowId].id = cowId;
+        cows.push(demCows[cowId]);
+      });
+      resolve(cows);
+    })
+    .catch((err) => reject(err));
+});
 
 export default { getCows };
